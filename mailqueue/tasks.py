aa4ef6a75cfe2f6d1afd586da67123cdb7ee27d5
Anton <mmodbrreciever@gmail.com>
2014-09-16 16:42:08 +0400

    djcelery

--- mailqueue/tasks.py
+++ mailqueue/tasks.py
@@ -1,7 +1,7 @@
-from celery.task import task
+from djcelery import celery
 from .models import MailerMessage
 
-@task(name="tasks.send_mail", default_retry_delay=5, max_retries=5)
+@celery.task(name="tasks.send_mail", default_retry_delay=5, max_retries=5)
 def send_mail(pk):
     message = MailerMessage.objects.get(pk=pk)
     message._send()
@@ -10,7 +10,7 @@ def send_mail(pk):
     if not message.sent:
         send_mail.retry([message.pk,])
 
-@task()
+@celery.task()
 def clear_sent_messages():
     from mailqueue.models import MailerMessage
     MailerMessage.objects.clear_sent_messages()
